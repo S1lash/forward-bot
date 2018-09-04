7c65a32eddc1b496b0d36b9ecc2f9a115843fbb3
Ilya Kuzmichev <mg20118@gmail.com>
2018-09-04 20:21:44 +0300

    first commit

---
+++ src/main/java/ru/kuzmichev/forwardbot/ForwardBotApplication.java
@@ -0,0 +1,12 @@
+package ru.kuzmichev.forwardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForwardBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForwardBotApplication.class, args);
+	}
+}
